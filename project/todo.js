3f17ee2f204ade6403c9d3ff3f133e1a1fbeb87f
Raju Ozarkar <rajuozarkar01@gmail.com>
2024-08-22 11:09:37 +0530

    To Do App Complete

--- project/todo.js
+++ project/todo.js
@@ -1,5 +1,38 @@
+let todoList = [
+  {item :'Buy Milk', 
+   dueDate: '21/8/2024'
+  },
+  {item:'Go to Collage',
+   dueDate: '21/8/2024'
+  }
+];
+displayItems();
+
 function addTodo() {
-let inputElement = document.querySelector('#todo-input');
-let todoItem = inputElement.value;
-console.log(todoItem);
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+  displayItems();
+}
+
+function displayItems(){
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+  for (let i = 0; i < todoList.length; i++){
+     let {item, dueDate} = todoList[i];
+    newHtml +=`
+   
+    <span>${item}</span>
+    <span>${dueDate}</span>
+    <button class="button-shadow red-back" onclick ="todoList.splice(${i}, 1);
+    displayItems();">Delete</button>
+   
+    `;
+    //why span: div, p  are block elements
+  }
+  containerElement.innerHTML = newHtml;
 }
